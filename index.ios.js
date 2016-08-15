6b0fc0071cf804e099da0675b057552f8269a79e
Justin Anderson <janderson2k@gmail.com>
2016-08-14 21:19:40 -0700

    displays one time password and time countdown

--- index.ios.js
+++ index.ios.js
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+import generateOTP from './lib/totp.js';
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -12,20 +12,45 @@ import {
   View
 } from 'react-native';
 
-class TotpHat extends Component {
+const TotpHat = React.createClass({
+  getInitialState() {
+    return {
+      generators: [
+        {
+          name: 'Gmail <js-ios@gmail.com>',
+          secret: 'abcdefghijklmnop'
+        }
+      ]
+    }
+  },
+
+  componentDidMount(){
+    var updateInterval = setInterval( () =>{
+      this.forceUpdate();
+    }, 1000);
+  },
+
+  renderGenerator(generator) {
+    const { otp, secondsBeforeExpiration } = generateOTP(generator)
+    // `otp` is the One Time Password, that is the code to be used as a second factor
+    // `secondsBeforeExpiration` is an integer representation of how much longer (in seconds) the otp is valid
+
+    return (
+      <View key={generator.name}>
+        <Text>{ generator.name } | { otp } | { secondsBeforeExpiration }</Text>
+      </View>
+    )
+  },
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to TOTP!
-        </Text>
-        <Text style={styles.instructions}>
-          The best app ever!
-        </Text>
+        { this.state.generators.map(this.renderGenerator) }
       </View>
-    );
-  }
-}
+    )
+  },
+
+})
 
 const styles = StyleSheet.create({
   container: {
